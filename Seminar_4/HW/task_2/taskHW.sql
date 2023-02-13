cd0aec24ff0735a77f33233d4ebafaea7a4a1092
Royal00Blood <nazgoool1@gmail.com>
2023-02-13 18:24:31 +0400

    hw4 complit

--- Seminar_4/HW/task_2/taskHW.sql
+++ Seminar_4/HW/task_2/taskHW.sql
@@ -8,3 +8,26 @@
 --"от 7, где 7 не попадает в данный диапазон, до 9 включительно". 
 --Такой синтаксис нельзя использовать в sql, вам нужно найти решение, как такое условие можно записать в sql по-другому.
 --Для проверок работы своего скрипта можете использовать свою базу данных, либо используйте сайт: https://onecompiler.com/mysql
+
+-- create
+CREATE TABLE STUDENTS (
+  empId INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name TEXT NOT NULL,
+  age INTEGER NOT NULL,
+  addres TEXT NOT NULL
+);
+
+-- insert 
+INSERT INTO STUDENTS (name, age, addres) VALUES ('Clark', 17, 'Москва');
+INSERT INTO STUDENTS (name, age, addres) VALUES ('Jon', 11, 'Москва');
+INSERT INTO STUDENTS (name, age, addres) VALUES ('Boby', 22, 'Наро-Фоминск');
+INSERT INTO STUDENTS (name, age, addres) VALUES ('Charly', 25, 'Красногорск');
+INSERT INTO STUDENTS (name, age, addres) VALUES ('Klark', 24, 'Москва');
+
+-- fetch 
+SELECT name FROM STUDENTS  WHERE age >= 18 and age < 30 and addres = 'Москва';
+
+--Output:
+
+--name
+--Klark
